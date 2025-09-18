e3aaa61f8de586eac0476b861b44910d65daee9a
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-18 11:10:08 +0530

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#f0f9ff',
+          100: '#e6f7ff',
+          500: '#0095f6',
+          700: '#0077d1'
+        }
+      }
+    }
+  },
+  plugins: []
+}
